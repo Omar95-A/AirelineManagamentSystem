41bc834cb92bf79afa42707e72c3369e23e7ed70
Omar95-A <81801245+Omar95-A@users.noreply.github.com>
2022-01-20 18:43:28 -0800

    2

---
+++ Elfa Airline Projesi/‏‏‏‏AirlineMS/src/airlinems/DbUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airlinems;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author OmarA
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModel(ResultSet Rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
